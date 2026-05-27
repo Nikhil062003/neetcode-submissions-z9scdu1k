c68e8c0b4d00dce97a05ba4b696e57343fbac28a
NIKHIL SHARMA <77260834+Nikhil062003@users.noreply.github.com>
2026-05-27 11:32:12 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int sum =1;int count=0;
+
+        for(int i=0;i<nums.length;i++){
+            if( nums[i] == 0 ){
+                count++;
+                continue;
+            }
+            sum *= nums[i];
+            
+        }
+        // System.out.println(sum);
+        if(count>=2){
+            return new int[nums.length];
+        }
+        for(int i=0;i<nums.length;i++){
+            if(count == 1 && nums[i] != 0 ){
+                nums[i]=0;
+                
+            }else if(nums[i]==0){
+                nums[i]=sum;
+                
+            }else{            nums[i]=sum/nums[i];}
+        }
+        return nums;
+       
+    }
+}  
